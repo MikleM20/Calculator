563c08247f5c5c855d4a1b3bc35f275d23897395
MikleM20 <40878852+MikleM20@users.noreply.github.com>
2020-02-13 15:37:37 -0700

    Act Like Google Calculator

--- calc.js
+++ calc.js
@@ -1,33 +1,52 @@
 
 //function that display value
-let ans = 0;
-function dis(val){
-document.getElementById("input").value+=val;
+let changedAfterSolve = 0;
+function disNum(val){
+if(changedAfterSolve == 0){
+	document.getElementById("input").value="";
+	document.getElementById("input").value+=val;
+	changedAfterSolve = 1;
+	}
+else{
+	document.getElementById("input").value+=val;
+	}
 }
 
+function disOp(val){
+document.getElementById("input").value+=val;
+changedAfterSolve = 1;
+}
 //function that evaluates the digit and return input
 function solve(){
-	let x = document.getElementById("input").value;
-	try{
-		let y = eval(x);
-		document.getElementById("result").value = document.getElementById("input").value;
-		document.getElementById("result").value +='=';
-		document.getElementById("result").value += y;
-		document.getElementById("input").value = y;
+	if(changedAfterSolve == 0){
+		document.getElementById("result").value = '0';
+		document.getElementById("input").value = "";
 	}
-	catch (e) {
-		if (e instanceof SyntaxError) {
-			document.getElementById("result").value = "Error";
+	else{
+		let x = document.getElementById("input").value;
+		try{
+			let y = eval(x);
+			document.getElementById("result").value = document.getElementById("input").value;
+			document.getElementById("result").value +='=';
+			document.getElementById("result").value += y;
+			document.getElementById("input").value = y;
+			changedAfterSolve = 0;
 		}
-    }
+		catch (e) {
+			if (e instanceof SyntaxError) {
+				document.getElementById("result").value = "Error";
+			}
+		}
+	}
 }
 
 //function that clear the display
 function clr(){
-document.getElementById("input").value = "";
-document.getElementById("result").value = "";
+	document.getElementById("input").value = "";
+	document.getElementById("result").value = "";
 }
 
 function bkspc(){
-document.getElementById("input").value = document.getElementById("input").value.slice(0, document.getElementById("input").value.length - 1);
+	document.getElementById("input").value = document.getElementById("input").value.slice(0, document.getElementById("input").value.length - 1);
+	changedAfterSolve = 1;
 }
